4d59adfa3eea445dfd21e10342bbb4c538825b76
choi-yuna <mjung0226@naver.com>
2024-10-24 16:40:39 +0900

    치주 차트 작업

--- src/pages/analysisView/analysis.jsx
+++ src/pages/analysisView/analysis.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import ToothChart from '../../components/toothChart';
+import PeriodontalChart from '../../components/PeriodontalChart';
+import  {UploadContext}  from '../../context/UploadContext';
+
+const FileUpload = () => {
+  const { handleFolderUpload } = useContext(UploadContext);
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    handleFolderUpload(files);
+  };
+
+  return (
+    <div>
+      <input type="file" webkitdirectory="true" onChange={handleFileChange} />
+    </div>
+  );
+};
 
 const Analysis = () => {
+  
   return (
     <Container>
       <ToothChart/>
+      <PeriodontalChart />
     </Container>
   );
 };
