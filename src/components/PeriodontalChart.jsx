b87c996a1f1c458b690d26119513da08fdb3bb93
DESKTOP-BSAE3OV\fasol <wlsdn4348@naver.com>
2024-11-04 10:03:44 +0900

    치아 번호 수정

--- src/components/PeriodontalChart.jsx
+++ src/components/PeriodontalChart.jsx
@@ -10,7 +10,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} 
+from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { useIniDataContext } from '../context/IniDataContext';
 
@@ -50,7 +51,7 @@ const PeriodontalChart = () => {
     
       const teethOrder = isMaxillary
         ? [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28]
-        : [38, 37, 36, 35, 34, 33, 32, 31, 41, 42, 43, 44, 45, 46, 47, 48];
+        : [48, 47, 46, 45, 44, 43, 42,41, 31, 32, 33, 34, 35, 36, 37, 38];
     
       teethOrder.forEach((toothNum) => {
         const toothData = parsedData[toothNum];
